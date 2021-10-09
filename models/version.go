982e7b38cc8d85c72359d7bec13f9475b298e760
遐迩 <xiaer@xiaero.cn>
2021-10-09 10:26:27 +0800

    .

--- models/version.go
+++ models/version.go
@@ -11,7 +11,7 @@ import (
 	"github.com/beego/beego/v2/core/logs"
 )
 
-var version = "2021100901"
+var version = "2021100902"
 var describe = "日常更新"
 var AppName = "xdd"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
