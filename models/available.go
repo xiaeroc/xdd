8eff6b8d47234977f99a22060fc8fb5c8ba72679
xiaer <xiaer@xiaero.cn>
2021-10-10 11:55:21 +0800

    .

--- models/available.go
+++ models/available.go
@@ -143,7 +143,6 @@ func CookieOK(ck *JdCookie) bool {
 	if ck == nil {
 		return true
 	}
-	logs.Info("ck----------------------ck" + cookie)
 	req := httplib.Get("https://me-api.jd.com/user_new/info/GetJDUserInfoUnion")
 	req.Header("Cookie", cookie)
 	req.Header("Accept", "*/*")
@@ -157,14 +156,11 @@ func CookieOK(ck *JdCookie) bool {
 	if err != nil {
 		return false
 	}
-	s, err := req.String()
-	logs.Info("req---------------------------", s)
-	logs.Info("req---------------------err-", err)
+	_, err = req.String()
 	ui := &UserInfoResult{}
 	if nil != json.Unmarshal(data, ui) {
 		return false
 	}
-	logs.Info("ui---------------------------", ui)
 	switch ui.Retcode {
 	case "1001": //ck.BeanNum
 		if ui.Msg == "not login" {
