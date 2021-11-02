25d90344c258ea709c9f337c4b5b1d09255e612e
遐迩 <xiaer@xiaero.cn>
2021-11-02 15:33:56 +0800

    .

--- controllers/other.go
+++ controllers/other.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/beego/beego/v2/core/logs"
+import (
+	"fmt"
+	"github.com/beego/beego/v2/core/logs"
+	"time"
+)
 
 type OtherController struct {
 	BaseController
@@ -10,6 +14,7 @@ func (c *OtherController) AuthToken() {
 	logs.Info(c.Ctx.Request.GetBody)
 	logs.Info(c.Ctx.Request.URL)
 	logs.Info(c.Ctx.Request.PostForm)
+	c.Ctx.WriteString(fmt.Sprintf("{\"code\":200,\"data\":{\"token\":\"3e60261b-dece-4a1e-942c-690ba7205f76\",\"token_type\":\"Bearer\",\"expiration\":%d}}", time.Now().Unix()+2626560))
 }
 
 func (c *OtherController) Envs() {
