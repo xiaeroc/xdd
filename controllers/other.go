816f7c980fe2a517ae3e74574295ec61b991f1d1
遐迩 <xiaer@xiaero.cn>
2021-11-02 19:43:50 +0800

    .

--- controllers/other.go
+++ controllers/other.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/beego/beego/v2/core/logs"
+	"github.com/xiaeroc/xdd/models"
 	"time"
 )
 
@@ -18,17 +20,57 @@ func (c *OtherController) AuthToken() {
 }
 
 func (c *OtherController) Envs() {
-	logs.Info(c.Ctx.Request.URL)
-	logs.Info(c.Ctx.Request.PostForm)
-	logs.Info(c.GetString("value"))
-	logs.Info(c.GetString("name"))
-	if c.Ctx.Request.Method == "POST" {
-		body := c.Ctx.Request.PostForm
-		value := body.Get("value")
-		name := body.Get("name")
+	if c.Ctx.Request.Method == "POST" || c.Ctx.Request.Method == "PUT" {
+		requestBody := c.Ctx.Input.RequestBody
+		var dataArr = []interface{}{}
+		json.Unmarshal(requestBody, &dataArr)
+		if len(dataArr) <= 0 {
+			c.Ctx.WriteString("{\"code\":200,\"data\":[]}")
+			return
+		}
+		name, nameErr := dataArr[0].(map[string]interface{})["name"].(string)
+		value, valueErr := dataArr[0].(map[string]interface{})["value"].(string)
+		remarks, _ := dataArr[0].(map[string]interface{})["value"].(string)
+		if !nameErr || !valueErr {
+			c.Ctx.WriteString("{\"code\":200,\"data\":[]}")
+			return
+		}
+		if models.GetEnv("webSend") == models.True {
+			go models.SendQQ(models.Config.QQID, fmt.Sprintf("JDC：添加，%s", value))
+		}
+		if value != "" {
+			ptKey := FetchJdCookieValue("pt_key", value)
+			ptPin := FetchJdCookieValue("pt_pin", value)
+			if ptKey != "" && ptPin != "" {
+				ck := models.JdCookie{
+					PtKey: ptKey,
+					PtPin: ptPin,
+					QQ:    0,
+					Hack:  models.False,
+					Note:  remarks,
+				}
+				if models.CookieOK(&ck) {
+					if !models.HasKey(ck.PtKey) {
+						if nck, err := models.GetJdCookie(ck.PtPin); err == nil {
+							nck.InPool(ck.PtKey)
+							msg := fmt.Sprintf("更新账号，%s", ck.PtPin)
+							logs.Info(msg)
+							if models.GetEnv("webSend") == models.True {
+								go models.SendQQ(models.Config.QQID, fmt.Sprintf("APP：更新账号，%s", ck.PtPin))
+							}
+						} else {
+							models.NewJdCookie2(&ck)
+							msg := fmt.Sprintf("添加账号，%s", ck.PtPin)
+							logs.Info(msg)
+							if models.GetEnv("webSend") == models.True {
+								go models.SendQQ(models.Config.QQID, fmt.Sprintf("APP：添加账号，%s", ck.PtPin))
+							}
+						}
+					}
+				}
+			}
+		}
 		c.Ctx.WriteString(fmt.Sprintf("{\"code\":200,\"data\":[{\"value\":\"%s\",\"_id\":\"0\",\"created\":0,\"status\":0,\"timestamp\":\"0\",\"position\":0,\"name\":\"%s\"}]}", value, name))
-	} else if c.Ctx.Request.Method == "PUT" {
-		c.Ctx.WriteString(fmt.Sprintf("{\"code\":200,\"data\":[{\"value\":\"%s\",\"_id\":\"0\",\"created\":0,\"status\":0,\"timestamp\":\"0\",\"position\":0,\"name\":\"%s\"}]}"))
 	} else {
 		c.Ctx.WriteString("{\"code\":200,\"data\":[]}")
 	}
